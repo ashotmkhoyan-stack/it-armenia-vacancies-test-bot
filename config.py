3799f40c3d99596643468dadcc3d9e871c0daa8b
ashotmkhoyan <ashotmkhoyan@gmail.com>
2026-04-07 22:09:30 +0400

    Fix CHANNEL_ID environment variable format

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
-CHANNEL_ID: str = os.getenv("CHANNEL_ID", "@@it_armenia_vacancies_test")
+CHANNEL_ID: str = os.getenv("CHANNEL_ID", "@it_armenia_vacancies_test")
 SCRAPE_INTERVAL_MINUTES: int = int(os.getenv("SCRAPE_INTERVAL_MINUTES", "30"))
 MAX_POSTS_PER_BATCH: int = int(os.getenv("MAX_POSTS_PER_BATCH", "5"))
 DELAY_BETWEEN_POSTS: int = int(os.getenv("DELAY_BETWEEN_POSTS", "30"))
